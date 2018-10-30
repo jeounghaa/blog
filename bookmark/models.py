a17b52d59245dc5c620fe819f4b92a1b87b752a2
jha22 <dnfken@naver.com>
2018-10-30 10:37:49 +0900

    Changed ...

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-
-# 장고에서 # 테이블은 (django.dbmodels.Model을 상속받는) 클래스로 정의하고, # 테이블 컬럼은 클래스 변수로 정의함
+# 장고에서
+# 테이블은 (django.dbmodels.Model을 상속받는) 클래스로 정의하고,
+# 테이블 컬럼은 클래스 변수로 정의함
 class Bookmark(models.Model):
     # id 필드(Integer, PK, Auto Increment)는 자동 생성됨
     title = models.CharField(max_length=100, blank=True, null=True)
